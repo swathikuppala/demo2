fa2410071f87cb3a62722cd930a8f1e03f2cf023
swathikuppala <kswathi515@gmail.com>
2022-02-24 09:16:08 +0530

    class file is added

---
+++ newdemo25/src/newdemo25/newdemo25class.java
@@ -0,0 +1,10 @@
+package newdemo25;
+
+public class newdemo25class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.print("Helloworld");
+	}
+
+}
